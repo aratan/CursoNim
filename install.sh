e3cde99bc0485a3e76cc63a3c08ebe2dafbdb3c6
Víctor <systemdeveloper868@gmail.com>
2022-06-12 09:27:27 +0200

    config variables de entorno

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ sh build.sh
 bin/nim c koch
 ./koch boot -d:release
 ./koch tools
+echo "Variables de entorno"
+cd ..
+cp -r ./nim-1.6.6 /usr/bin/
+#export PATH=/home/trendoceans/.nimble/bin:$PATH`
+nano .bashrc
+export PATH=/usr/bin/nim-1.6.6/bin:$PATH`
+source ~/.bashrc
+nim -v
 echo "TERMINADO"
-echo "~/.nimble/bin"
 
